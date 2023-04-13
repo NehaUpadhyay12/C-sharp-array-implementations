2140eeae65938f962b95ff5d12416dae522fb866
NehaUpadhyay12 <upaneha1212@gmail.com>
2023-04-13 16:38:57 +0530

    Initial Commit

---
+++ MarkTheAnswer/MarkTheAnswer/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string firstLine = Console.ReadLine();
+        int[] arr = firstLine.Split(' ').Select(int.Parse).ToArray();
+        int numberOfQuestions = arr[0];
+        int maxDifficulty = arr[1];
+
+        string questionsStr = Console.ReadLine();
+        int[] questions = questionsStr.Split(' ').Select(int.Parse).ToArray();
+
+        int attemptedQusetions = 0;
+        int skippedCount = 0;
+
+        for (int i = 0; i < numberOfQuestions; i++)
+        {
+            if (questions[i] > maxDifficulty && skippedCount < 2)
+            {
+                skippedCount++;
+                if (skippedCount == 2) break;
+            }
+            else
+            {
+                attemptedQusetions++;
+            }
+        }
+
+        Console.WriteLine(attemptedQusetions.ToString());
+    }
+}
